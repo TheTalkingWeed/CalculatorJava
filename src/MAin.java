84fe153665e3e52f8501b8daaf43aade52f10536
Alex_sz1 <61713353+TheTalkingWeed@users.noreply.github.com>
2022-08-11 13:06:16 +0200

    Add files via upload

---
+++ src/MAin.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MAin {
+
+    
+    public static void main(String[] args) throws IOException {
+
+
+    new Calculator();
+    }
+}
